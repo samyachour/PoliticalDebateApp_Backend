17b69a25159fb4b5c91b95863a693f8e905b99ea
Samy Achour <sachour96@gmail.com>
2019-02-23 20:25:14 -0800

    Generated and hid new secret key

--- PoliticalDebateApp/settings.py
+++ PoliticalDebateApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'emhkpc5-i7l0w%h)m-o7r$ncq+@petv001a7vskh#+s-1gy!go'
+SECRET_KEY = secretKeyHidden
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- build_venv.sh
+++ build_venv.sh
@@ -1,6 +1,6 @@
 # Shell script to create virtual environment
 
-virtualenv --python=python3 venv
+virtualenv --python=/usr/local/Cellar/python/3.7.2_2/bin/python3.7  venv
 source venv/bin/activate
 pip3 install Django
 pip3 install djangorestframework
