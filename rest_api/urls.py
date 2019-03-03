a2b7843a74010489b789630215d239e0e25e0796
Samy Achour <sachour96@gmail.com>
2019-03-03 01:22:01 -0800

    issue7 Setting up basic views, models, authenticaion, serializers, and tests

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import ListDebatesView
+from .views import ListDebatesView, LoginView, DebatesDetailView, ProgressView, RegisterUsers
 
 
 urlpatterns = [
-    path('debates/', ListDebatesView.as_view(), name="debates-all")
+    path('debates/', ListDebatesView.as_view(), name="debates-all"),
+    path('debates/<int:pk>/', DebatesDetailView.as_view(), name="get-debate"),
+    path('progress/<int:pk>/', ProgressView.as_view(), name="get-progress"),
+    path('progress/', ProgressView.as_view(), name="post-progress"),
+    path('auth/login/', LoginView.as_view(), name="auth-login"),
+    path('auth/register/', RegisterUsers.as_view(), name="auth-register")
 ]
