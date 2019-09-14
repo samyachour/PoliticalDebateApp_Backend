001c307be03160f94662ff11bb41b97728084694
Samy Achour <sachour96@gmail.com>
2019-09-14 10:59:37 -0700

    Updated debate map parser to work with new template

--- rest_api/helpers/debate_map_parser.py
+++ rest_api/helpers/debate_map_parser.py
@@ -2,16 +2,16 @@ from django.contrib.auth.models import User
 from rest_api.models import *
 from .constants import *
 from datetime import datetime, timedelta
+from string import punctuation
 
 # Run in shell:
-# from rest_api.helpers.debate_map_parser import parse_debate_file
-# parse_debate_file("/Users/samy/Documents/PoliticalDebateApp/PoliticalDebateApp_Backend/Debate\ Map\ \(\).txt", old_version_pk); exit();
+# from rest_api.helpers.debate_map_parser import parse_debate_file; parse_debate_file("/Users/samy/Documents/PoliticalDebateApp/PoliticalDebateApp_Backend/DebateMap.txt"); exit();
 
 # Helpers
 
-def count_indentation(source_string, tabsize=4):
-    stripped_source_string = source_string.lstrip()
-    return stripped_source_string, len(source_string) - len(stripped_source_string)
+key_key = "key"
+main_key = "main"
+object_key = "object"
 
 def check_for_key(key, source_string):
     key_length = len(key)
@@ -21,13 +21,13 @@ def check_for_key(key, source_string):
 def get_value_for_key(key, source_string):
     key_length = len(key)
     value_string = source_string[key_length:]
-    return value_string.replace(": ","",1).rstrip()
+    return value_string.replace(":","",1).lstrip().rstrip()
 
 def basic_debate_info_complete(debate_info_dict):
     return title_key in debate_info_dict and short_title_key in debate_info_dict and tags_key in debate_info_dict and last_updated_key in debate_info_dict and total_points_key in debate_info_dict
 
 def basic_point_info_complete(point_info_dict):
-    return description_key in point_info_dict and short_description_key in point_info_dict and side_key in point_info_dict
+    return main_key in point_info_dict and key_key in point_info_dict and description_key in point_info_dict and short_description_key in point_info_dict and side_key in point_info_dict and rebuttals_key in point_info_dict
 
 def parse_hyperlinks(description):
     cleaned_description = description
@@ -62,7 +62,7 @@ def parse_hyperlinks(description):
     return cleaned_description, hyperlinks
 
 def parse_images(point, images):
-    images_list = images.split("||")
+    images_list = images.split(" || ")
     for image in images_list:
         image_parts = image.split("//")
         image_dict = {}
@@ -89,10 +89,10 @@ def parse_images(point, images):
 
 # Parser
 
-def parse_debate_file(filename, old_version_pk = -1):
+def parse_debate_file(filename, title_for_deletion = ""):
 
-    if old_version_pk != -1:
-        Debate.objects.filter(pk=old_version_pk).delete()
+    if title_for_deletion != "":
+        Debate.objects.filter(title=title_for_deletion).delete()
 
     # Properties
 
@@ -102,19 +102,18 @@ def parse_debate_file(filename, old_version_pk = -1):
     debate_info_dict[total_points_key] = point_count
 
     point_info_dict = {}
-    indented_points_dict = {}
+    all_points_dict = {}
     is_parsing_points = False
-    whitespace_count = 0
-    typical_whitespace = 0
 
     # Parsing
 
     with open(filename) as debate_map:
 
         for line in debate_map:
-            line = line.replace("- ","",1)
             if line in ['\n', '\r\n']: # empty line
                 continue
+            # Trim all leading non-alphanumeric characters
+            line = line.lstrip().lstrip(punctuation).lstrip()
 
             if not is_parsing_points: # basic info section
                 if check_for_key(title_key, line):
@@ -131,12 +130,11 @@ def parse_debate_file(filename, old_version_pk = -1):
                     new_debate = Debate.objects.create(title=debate_info_dict[title_key], short_title=debate_info_dict[short_title_key], tags=debate_info_dict[tags_key], last_updated=debate_info_dict[last_updated_key], total_points=debate_info_dict[total_points_key])
 
             if is_parsing_points: # points section
-                previous_whitespace_count = whitespace_count
-                line, whitespace_count = count_indentation(line)
-                if whitespace_count != previous_whitespace_count:
-                    typical_whitespace = whitespace_count - previous_whitespace_count
-
-                if check_for_key(description_key, line):
+                if check_for_key(main_key, line):
+                    point_info_dict[main_key] = True
+                elif check_for_key(key_key, line):
+                    point_info_dict[key_key] = int(get_value_for_key(key_key, line))
+                elif check_for_key(description_key, line):
                     description = get_value_for_key(description_key, line)
                     description, point_info_dict[hyperlinks_key] = parse_hyperlinks(description)
                     point_info_dict[description_key] = description
@@ -144,26 +142,38 @@ def parse_debate_file(filename, old_version_pk = -1):
                     point_info_dict[short_description_key] = get_value_for_key(short_description_key, line)
                 elif check_for_key(side_key, line):
                     point_info_dict[side_key] = get_value_for_key(side_key, line)
+                elif check_for_key(rebuttals_key, line):
+                    rebuttals = get_value_for_key(rebuttals_key, line)
+                    formatted_rebuttals = []
+                    if rebuttals:
+                        rebuttals = rebuttals.split(", ")
+                        for rebuttal_pk in rebuttals:
+                            formatted_rebuttals.append(int(rebuttal_pk))
+                    point_info_dict[rebuttals_key] = formatted_rebuttals
                 elif check_for_key(images_key, line):
-                    current_point = indented_points_dict[whitespace_count]
                     images = get_value_for_key(images_key, line)
-                    parse_images(current_point, images)
+                    parse_images(point_info_dict[object_key], images)
 
                 if basic_point_info_complete(point_info_dict):
-                    if whitespace_count == 0:
+                    if point_info_dict[main_key]:
                         new_point = Point.objects.create(debate=new_debate, description=point_info_dict[description_key], short_description=point_info_dict[short_description_key],  side=point_info_dict[side_key])
                     else:
-                        parent_point = indented_points_dict[whitespace_count - typical_whitespace]
                         new_point = Point.objects.create(description=point_info_dict[description_key], short_description=point_info_dict[short_description_key],  side=point_info_dict[side_key])
-                        parent_point.rebuttals.add(new_point)
-                        parent_point.save()
 
                     for hyperlink in point_info_dict[hyperlinks_key]:
                         PointHyperlink.objects.create(point=new_point, substring=hyperlink[substring_key], url=hyperlink[url_key])
 
-                    indented_points_dict[whitespace_count] = new_point
+                    point_info_dict[object_key] = new_point
+                    all_points_dict[point_info_dict[key_key]] = point_info_dict
                     point_count += 1
-                    point_info_dict = {}
+                    point_info_dict = { main_key: False, object_key: new_point }
+
+    for _, point in all_points_dict.items():
+        point_object = point[object_key]
+        for rebuttal_pk in point[rebuttals_key]:
+            rebuttal = all_points_dict[rebuttal_pk][object_key]
+            point_object.rebuttals.add(rebuttal)
+        point_object.save()
 
     debate_info_dict[total_points_key] = point_count
     new_debate.total_points = point_count
